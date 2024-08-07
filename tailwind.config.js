3d681918409c8e46dea209a33b4022ae1a12b46a
gattomattew <gattomattew@gmail.com>
2024-08-08 00:12:33 +0700

    fix: build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./Components/**/*.{html,js,php,twig}",
-    "./templates/**/*.{html,js,php,twig}",
+    './Components/**/*.{html,js,php,twig}',
+    './templates/**/*.{html,js,php,twig}'
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#003268",
-        secondary: "#4A85DD",
-        warning: "#FF6846",
-        dark: "#00162D",
-        gray: "#64626A",
+        primary: '#003268',
+        secondary: '#4A85DD',
+        warning: '#FF6846',
+        dark: '#00162D',
+        gray: '#64626A'
       },
       borderRadius: {
-        DEFAULT: "6px",
-      },
+        DEFAULT: '6px'
+      }
     },
     fontFamily: {
-      display: ["LibreBaskerville", "sans-serif"],
-      body: ["Inter", "sans-serif"],
-    },
+      display: ['LibreBaskerville', 'sans-serif'],
+      body: ['Inter', 'sans-serif']
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
